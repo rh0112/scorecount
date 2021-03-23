f941edb96cee715cfbdb83527f498cd614260953
rh0112 <66927763+rh0112@users.noreply.github.com>
2021-03-23 12:50:59 +0900

    Add files via upload
    swファイルの編集

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,9 @@
 // Cache name
-const CACHE_NAME = 'pwa-sample-caches-v1';
+const CACHE_NAME = 'tennis score counter';
 // Cache targets
 const urlsToCache = [
   './',
   './index.html',
-  './pages/a.html',
-  './pages/b.html',
-  './pages/c.html',
-  './css/style.css',
-  './images/a.jpg',
-  './images/b.jpg',
-  './images/c.jpg',
 ];
 
 self.addEventListener('install', (event) => {
